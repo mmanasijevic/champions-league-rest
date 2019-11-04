c7d853d2aefde0c5cdfc959380bac5e33cf37dcf
Marko Manasijevic <marko.manasijevic@gmail.com>
2019-11-04 10:19:09 +0100

    Inital commit

---
+++ src/main/java/net/jdevelopment/championsleaguerest/ChampionsLeagueRestApplication.java
@@ -0,0 +1,13 @@
+package net.jdevelopment.championsleaguerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionsLeagueRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionsLeagueRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jdevelopment/championsleaguerest/ChampionsLeagueRestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jdevelopment.championsleaguerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionsLeagueRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
